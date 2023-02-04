7002a242a143ba74c3e1bf2925aaf641d327ac29
畑山翔斗 <>
2023-02-04 17:47:21 +0900

    インスタンス変数を全てfinalに変更 コンストラクタにTimestamp型をDateTime型に変更する処理追加 Timestamp型を変更した時のバリデーションを設定

--- lib/entity/public_offering.dart
+++ lib/entity/public_offering.dart
@@ -1,9 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PublicOffering {
-  String name;
-  String url;
-  Timestamp beginDate;
-  Timestamp endDate;
-  PublicOfferingModel(this.name, this.url, this.beginDate, this.endDate);
+  late final String name;
+  late final String url;
+  late final DateTime beginDate;
+  late final DateTime endDate;
+
+  PublicOffering(
+      String name, String url, Timestamp beginDate, Timestamp endDate) {
+    final convert_datetime_begindate = beginDate.toDate();
+    final convert_datetime_enddate = endDate.toDate();
+    if (convert_datetime_begindate == null) {
+      print("beginDateがDateTime型に変換できませんでした");
+      return;
+    }
+    if (convert_datetime_enddate == null) {
+      print("endDateがDateTime型に変換できませんでした");
+      return;
+    }
+    this.name = name;
+    this.url = url;
+    this.beginDate = convert_datetime_begindate;
+    this.endDate = convert_datetime_enddate;
+  }
 }
