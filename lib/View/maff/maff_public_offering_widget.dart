bd64779e6c13bcfbf2223017a434a31f304aa468
畑山翔斗 <>
2023-02-07 23:44:19 +0900

    項目タップでアプリ内ブラウザを起動

--- lib/View/maff/maff_public_offering_widget.dart
+++ lib/View/maff/maff_public_offering_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fund_book/model/public_offering_list_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MaffPublicOfferingWidget extends StatelessWidget {
   @override
@@ -12,15 +13,22 @@ class MaffPublicOfferingWidget extends StatelessWidget {
           builder: (context, model, child) {
             final public_offering = model.public_Offering;
             final list_public_offering = public_offering
-                .map((public_offering) => Card(
-                    child: ListTile(
-                        title: Text(public_offering.name),
-                        subtitle: Text(public_offering.getFormatedBeginDate() +
-                            "〜" +
-                            public_offering.getFormatedEndDate()),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(100)),
-                        ))))
+                .map((public_offering) => GestureDetector(
+                    onTap: () {
+                      launchUrl(Uri.parse(public_offering.url),
+                          mode: LaunchMode.inAppWebView);
+                    },
+                    child: Card(
+                        child: ListTile(
+                            title: Text(public_offering.name),
+                            subtitle: Text(
+                                public_offering.getFormatedBeginDate() +
+                                    "〜" +
+                                    public_offering.getFormatedEndDate()),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100)),
+                            )))))
                 .toList();
             return ListView(
               children: list_public_offering,
