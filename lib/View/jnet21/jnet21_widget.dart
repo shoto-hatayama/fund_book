8520b780682420edf3314f81d275d7d20ae9e3ee
畑山翔斗 <>
2023-02-09 19:21:18 +0900

    検索条件のモックを作成

--- lib/View/jnet21/jnet21_widget.dart
+++ lib/View/jnet21/jnet21_widget.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
 
-class Jnet21Widget extends StatelessWidget {
+class Jnet21Widget extends StatefulWidget {
+  @override
+  State<Jnet21Widget> createState() => _Jnet21WidgetState();
+}
+
+class _Jnet21WidgetState extends State<Jnet21Widget> {
+  // TODO：モックのため後程変数は変更する
+  String selectedValue1 = '補助金';
+  String selectedValue2 = '製造業';
+  String selectedValue3 = '宮城県';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('JNET21のページです。', style: TextStyle(fontSize: 20)),
-      ),
-    );
+        body: Column(
+      // TODO:ExpansionTileは項目ごとにクラスを作る予定
+      children: [
+        ExpansionTile(
+          title: Text('検索条件'),
+          children: [
+            Row(children: [
+              Text('種類：'),
+              Spacer(),
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(value: '補助金', child: Text('補助金')),
+                    DropdownMenuItem(value: '融資', child: Text('融資'))
+                  ],
+                  value: selectedValue1,
+                  onChanged: (String? value) {
+                    setState(() {
+                      selectedValue1 = value!;
+                    });
+                  })
+            ]),
+            Row(children: [
+              Text('業種：'),
+              Spacer(),
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(value: '製造業', child: Text('製造業')),
+                    DropdownMenuItem(value: 'サービス業', child: Text('サービス業'))
+                  ],
+                  value: selectedValue2,
+                  onChanged: (String? value) {
+                    setState(() {
+                      selectedValue2 = value!;
+                    });
+                  })
+            ]),
+            Row(children: [
+              Text('地域：'),
+              Spacer(),
+              DropdownButton(
+                  items: const [
+                    DropdownMenuItem(value: '宮城県', child: Text('宮城県')),
+                    DropdownMenuItem(value: '秋田県', child: Text('秋田県'))
+                  ],
+                  value: selectedValue3,
+                  onChanged: (String? value) {
+                    setState(() {
+                      selectedValue3 = value!;
+                    });
+                  })
+            ])
+          ],
+        )
+      ],
+    ));
   }
 }
