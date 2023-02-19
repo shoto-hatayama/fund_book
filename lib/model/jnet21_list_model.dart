adc975e8eb6859dc5a202d8e80c7658e429d7f89
畑山翔斗 <>
2023-02-19 18:20:24 +0900

    jner21のlistを作成するモデル作成

---
+++ lib/model/jnet21_list_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:fund_book/entity/jnet21.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Jnet21ListModel extends ChangeNotifier {
+  List<Jnet21> jnet21 = [];
+
+  Future<void> getJnet21() async {
+    final collection = await FirebaseFirestore.instance
+        .collection('JNET21_SUBSIDES_AND_FINANCING')
+        .get();
+    final jnet21 = collection.docs
+        .map((doc) => Jnet21(
+            doc['type'],
+            doc['title'],
+            doc['area'],
+            doc['industry'],
+            doc['executing_agency'],
+            doc['executing_agencyinfo'],
+            doc['start_date'],
+            doc['end_date'],
+            doc['detail_url'],
+            doc['detail_nurl_name']))
+        .toList();
+
+    this.jnet21 = jnet21;
+    notifyListeners();
+  }
+}
