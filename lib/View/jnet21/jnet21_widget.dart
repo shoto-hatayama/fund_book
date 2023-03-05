6432b334adc321b5769682980fd834350ecb5f24
畑山翔斗 <>
2023-03-05 23:03:39 +0900

    検索条件のウィジェットが繰り返し表示されないように修正

--- lib/View/jnet21/jnet21_widget.dart
+++ lib/View/jnet21/jnet21_widget.dart
@@ -26,87 +26,86 @@ class _Jnet21WidgetState extends State<Jnet21Widget> {
                       onTap: () {
                         // 後程実装
                       },
-                      child: Column(
-                        // TODO:ExpansionTileは項目ごとにクラスを作る予定
-                        children: [
-                          ExpansionTile(
-                            title: Text('検索条件'),
-                            children: [
-                              Row(children: [
-                                Text('種類：'),
-                                Spacer(),
-                                DropdownButton(
-                                    items: const [
-                                      DropdownMenuItem(
-                                          value: '補助金', child: Text('補助金')),
-                                      DropdownMenuItem(
-                                          value: '融資', child: Text('融資'))
-                                    ],
-                                    value: selectedValue1,
-                                    onChanged: (String? value) {
-                                      setState(() {
-                                        selectedValue1 = value!;
-                                      });
-                                    })
-                              ]),
-                              Row(children: [
-                                Text('業種：'),
-                                Spacer(),
-                                DropdownButton(
-                                    items: const [
-                                      DropdownMenuItem(
-                                          value: '製造業', child: Text('製造業')),
-                                      DropdownMenuItem(
-                                          value: 'サービス業', child: Text('サービス業'))
-                                    ],
-                                    value: selectedValue2,
-                                    onChanged: (String? value) {
-                                      setState(() {
-                                        selectedValue2 = value!;
-                                      });
-                                    })
-                              ]),
-                              Row(children: [
-                                Text('地域：'),
-                                Spacer(),
-                                DropdownButton(
-                                    items: const [
-                                      DropdownMenuItem(
-                                          value: '宮城県', child: Text('宮城県')),
-                                      DropdownMenuItem(
-                                          value: '秋田県', child: Text('秋田県'))
-                                    ],
-                                    value: selectedValue3,
-                                    onChanged: (String? value) {
-                                      setState(() {
-                                        selectedValue3 = value!;
-                                      });
-                                    })
-                              ])
-                            ],
-                          ),
+                      child:
                           // カードウィジェットにJNET21から取得した情報を入れる
                           // TODO:後で表示の仕方について検討する（LISTTILEのタイトルの左側が空欄できる）
                           Card(
                               child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ListTile(
-                                title: Text('タイトル'),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(100))),
-                              ),
-                              // 期間が存在しない時は「-」のみにする
-                              Text('期間：2022月1月1日〜2023年1月1日'),
-                              Text('地域：宮城県'),
-                            ],
-                          ))
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ListTile(
+                            title: Text('タイトル'),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(100))),
+                          ),
+                          // 期間が存在しない時は「-」のみにする
+                          Text('期間：2022月1月1日〜2023年1月1日'),
+                          Text('地域：宮城県'),
                         ],
-                      )))
+                      ))))
                   .toList();
-              return ListView(
-                children: list_jnet21,
+              return Column(
+                children: [
+                  ExpansionTile(
+                    title: Text('検索条件'),
+                    children: [
+                      Row(children: [
+                        Text('種類：'),
+                        Spacer(),
+                        DropdownButton(
+                            items: const [
+                              DropdownMenuItem(
+                                  value: '補助金', child: Text('補助金')),
+                              DropdownMenuItem(value: '融資', child: Text('融資'))
+                            ],
+                            value: selectedValue1,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedValue1 = value!;
+                              });
+                            })
+                      ]),
+                      Row(children: [
+                        Text('業種：'),
+                        Spacer(),
+                        DropdownButton(
+                            items: const [
+                              DropdownMenuItem(
+                                  value: '製造業', child: Text('製造業')),
+                              DropdownMenuItem(
+                                  value: 'サービス業', child: Text('サービス業'))
+                            ],
+                            value: selectedValue2,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedValue2 = value!;
+                              });
+                            })
+                      ]),
+                      Row(children: [
+                        Text('地域：'),
+                        Spacer(),
+                        DropdownButton(
+                            items: const [
+                              DropdownMenuItem(
+                                  value: '宮城県', child: Text('宮城県')),
+                              DropdownMenuItem(value: '秋田県', child: Text('秋田県'))
+                            ],
+                            value: selectedValue3,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedValue3 = value!;
+                              });
+                            })
+                      ])
+                    ],
+                  ),
+                  Expanded(
+                      child: ListView(
+                    children: list_jnet21,
+                  ))
+                ],
               );
             },
           ),
