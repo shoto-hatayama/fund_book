0ff5b73bfcbd2c69a788776dc6fd59497e0be010
畑山翔斗 <>
2023-04-22 16:47:35 +0900

    jnet21のTOP画面のリスト作成処理をmodelに切り出し

--- lib/View/jnet21/jnet21_widget.dart
+++ lib/View/jnet21/jnet21_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fund_book/entity/jnet21.dart';
 import 'package:fund_book/model/jnet21_list_model.dart';
 import 'package:fund_book/View/jnet21/jnet21_dropdown_widget.dart';
+import 'package:fund_book/model/jnet21_make_top_view.dart';
 import 'package:provider/provider.dart';
 
 class Jnet21Widget extends StatefulWidget {
@@ -21,29 +22,7 @@ class _Jnet21WidgetState extends State<Jnet21Widget> {
           body: Consumer<Jnet21ListModel>(
             builder: (context, model, child) {
               jnet21 = model.jnet21;
-              list_jnet21 = jnet21
-                  .map((jnet21) => GestureDetector(
-                      onTap: () {
-                        // 後程実装
-                      },
-                      child:
-                          // TODO:後で表示の仕方について検討する（LISTTILEのタイトルの左側が空欄できる）
-                          Card(
-                              child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ListTile(
-                            title: Text(jnet21.title),
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(100))),
-                          ),
-                          // 期間が存在しない時は「-」のみにする
-                          Text('期間：' + jnet21.termDate()),
-                          Text('地域：' + jnet21.area),
-                        ],
-                      ))))
-                  .toList();
+              list_jnet21 = Jnet21MakeTopView(jnet21).result;
               return Column(
                 children: [
                   Jnet21DropdownWidget(),
---
+++ lib/model/jnet21_make_top_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fund_book/entity/jnet21.dart';
+
+class Jnet21MakeTopView {
+  final List<Jnet21> _jnet21List;
+  List<GestureDetector> result = [];
+
+  Jnet21MakeTopView(this._jnet21List) {
+    this.result = this
+        ._jnet21List
+        .map((jnet21) => GestureDetector(
+            onTap: () {
+              // 後程実装
+            },
+            child:
+                // TODO:後で表示の仕方について検討する（LISTTILEのタイトルの左側が空欄できる）
+                Card(
+                    child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(jnet21.title),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(100))),
+                ),
+                // 期間が存在しない時は「-」のみにする
+                Text('期間：' + jnet21.termDate()),
+                Text('地域：' + jnet21.area),
+              ],
+            ))))
+        .toList();
+  }
+}
