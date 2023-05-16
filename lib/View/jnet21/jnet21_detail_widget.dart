b235e4c8aeaba81413e3828009f046c01cc6a913
畑山翔斗 <>
2023-05-16 14:56:20 +0900

    詳細画面のリストのモック作成

--- lib/View/jnet21/jnet21_detail_widget.dart
+++ lib/View/jnet21/jnet21_detail_widget.dart
@@ -9,8 +9,27 @@ class Jnet21DetailWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('')),
-        body: Center(
-          child: Text('jnet21の詳細ページです'),
+        body: Column(
+          children: [
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                      title: Text(
+                    'タイトル',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                  Text('内容')
+                ],
+              ),
+              color: Colors.green[100],
+              margin: const EdgeInsets.all(10),
+              elevation: 8,
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ],
         ));
   }
 }
