40ee6c4c065fe3abe4f5dc15ab68aeeeb536dccb
畑山翔斗 <>
2023-05-16 15:28:34 +0900

    詳細情報を作成するための項目名を定数クラスとして作成

---
+++ lib/const/jnet21_item_name.dart
@@ -0,0 +1,13 @@
+class Jnet21ItemName {
+  // jnet21のエンティティに対応する項目名
+  // detailUrlがないのは項目名として使わないから
+  static const String title = '資金名';
+  static const String area = '地域';
+  static const String type = '種類';
+  static const String industry = '業種';
+  static const String executingAgency = '実施機関';
+  static const String executingAgencyInfo = '実施機関からのお知らせ';
+  static const String startDate = '開始日';
+  static const String endDate = '終了日';
+  static const String detailUrlName = '詳細情報';
+}
