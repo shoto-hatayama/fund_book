96d098da947fd7ba7872ec67d0d870e32864e0be
畑山翔斗 <>
2023-05-16 16:44:03 +0900

    DetailCardクラスを使った実装に変更 項目が多いためスクロールできるように修正 paddingを設定

--- lib/View/jnet21/jnet21_detail_widget.dart
+++ lib/View/jnet21/jnet21_detail_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fund_book/const/jnet21_item_name.dart';
 import 'package:fund_book/entity/jnet21.dart';
+import 'package:fund_book/View/jnet21/detail_card.dart';
 
 class Jnet21DetailWidget extends StatelessWidget {
   final Jnet21 jnet21;
@@ -9,27 +11,25 @@ class Jnet21DetailWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('')),
-        body: Column(
-          children: [
-            Card(
-              child: Column(
-                children: const [
-                  ListTile(
-                      title: Text(
-                    'タイトル',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )),
-                  Text('内容')
-                ],
-              ),
-              color: Colors.green[100],
-              margin: const EdgeInsets.all(10),
-              elevation: 8,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              DetailCard(Jnet21ItemName.title, jnet21.title).make(),
+              DetailCard(Jnet21ItemName.expirationDate, jnet21.termDate())
+                  .make(),
+              DetailCard(Jnet21ItemName.type, jnet21.type).make(),
+              DetailCard(Jnet21ItemName.area, jnet21.area).make(),
+              DetailCard(Jnet21ItemName.industry, jnet21.industry).make(),
+              DetailCard(Jnet21ItemName.executingAgency, jnet21.executingAgency)
+                  .make(),
+              DetailCard(Jnet21ItemName.executingAgencyInfo,
+                      jnet21.executingAgencyInfo)
+                  .make(),
+              // TODO:URLをタップすると該当ページに遷移する処理を実装する
+              // 現在はタップしても遷移しない
+              DetailCard(Jnet21ItemName.detailUrlName, jnet21.detailUrl).make(),
+            ],
+          ),
         ));
   }
 }
