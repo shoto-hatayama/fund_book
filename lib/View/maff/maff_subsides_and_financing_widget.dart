763543cd2ac6733190134a54b298e7e2b66fc2c9
畑山翔斗 <>
2023-06-03 18:22:27 +0900

    モックをStatefulWidgetを使ったものに差し替え

--- lib/View/maff/maff_subsides_and_financing_widget.dart
+++ lib/View/maff/maff_subsides_and_financing_widget.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class MaffSubsidesAndFinancingWidget extends StatelessWidget {
+class MaffSubsidesAndFinancingWidget extends StatefulWidget {
+  @override
+  State<MaffSubsidesAndFinancingWidget> createState() =>
+      _SubsidesAndFinancingWidget();
+}
+
+class _SubsidesAndFinancingWidget
+    extends State<MaffSubsidesAndFinancingWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('補助金・融資', style: TextStyle(fontSize: 20)),
-      ),
-    );
+        body: Center(
+      child: Text('補助金・融資', style: TextStyle(fontSize: 20)),
+    ));
   }
 }
