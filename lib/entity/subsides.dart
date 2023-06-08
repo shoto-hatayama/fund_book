80e6d6c72d295416fe6af9876085bead95b2d2c7
畑山翔斗 <>
2023-06-08 23:04:53 +0900

    補助金を取り扱うクラスを作成

---
+++ lib/entity/subsides.dart
@@ -0,0 +1,12 @@
+class Subsides {
+  final String title; // タイトル
+  final String outline; // 概要
+  final String season; // 公募時期
+  final String rate; // 補助率
+  final String target; // 対象者
+  final String remarks; // 備考
+  final String url; // 参考url
+
+  Subsides(this.title, this.outline, this.season, this.rate, this.target,
+      this.remarks, this.url);
+}
