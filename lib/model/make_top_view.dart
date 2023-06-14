17a0549d3b1332fd450f60f84d632ea633095728
畑山翔斗 <>
2023-06-14 22:13:43 +0900

    融資情報用の表示処理作成

--- lib/model/make_top_view.dart
+++ lib/model/make_top_view.dart
@@ -7,5 +7,36 @@ class MakeTopView extends ChangeNotifier {
   List<GestureDetector> result = [];
   final BuildContext context;
 
-  MakeTopView(this.context) {}
+  MakeTopView(this.context) {
+    loadFundingList();
+  }
+
+  Future<void> loadFundingList() async {
+    final financing = FinancingList();
+    await financing.getFinancing();
+    _fundingList = financing.getList();
+  }
+
+  void financingTopView(List<Financing> fundingList) {
+    result = fundingList
+        .map((financing) => GestureDetector(
+            onTap: () {
+              // 後程実装
+            },
+            child: Card(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  title: Text(financing.title),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(100))),
+                ),
+                Text('借入限度額' + financing.borrowingLimit),
+                Text('償還期限：' + financing.termOfRedemption),
+              ],
+            ))))
+        .toList();
+    notifyListeners();
+  }
 }
