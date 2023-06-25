978530d7a7e3b417b4b80ca55b54717d3a9be98b
畑山翔斗 <>
2023-06-25 15:56:28 +0900

    項目名を持つ定数クラスを作成

---
+++ lib/const/financing_item_name.dart
@@ -0,0 +1,9 @@
+class FinancingItemName {
+  static const String title = '資金名';
+  static const String outline = '概要';
+  static const String target = '対象者';
+  static const String interest = '金利';
+  static const String borrowingLimit = '借入限度額';
+  static const String termOfRedemption = '償還期限';
+  static const String remarks = '備考';
+}
---
+++ lib/const/subsides_item_name.dart
@@ -0,0 +1,8 @@
+class SubsidesItemName {
+  static const String title = '資金名';
+  static const String outline = '概要';
+  static const String season = '公募時期';
+  static const String rate = '補助率';
+  static const String target = '対象者';
+  static const String remarks = '備考';
+}
